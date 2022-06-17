1f56373d6a913b5b84d37f76f71a56767f99c42e
Dekshut <denismaloffev@gmail.com>
2022-06-17 22:35:45 +0300

    updated AppSlice

--- client/src/redux/slices/appSlice.js
+++ client/src/redux/slices/appSlice.js
@@ -1,11 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getFavoriten = createAsyncThunk(
+  'app/favoriten',
+  async (userId, thunkAPI) => {
+    const favorites = await fetch(`http://localhost:8080/api/fav/${userId}`).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+
+      return response.json().then(error => {
+        const e = new Error('Smth gone wrong')
+        e.data = error
+        throw e
+      })
+    });
+
+    return favorites.data
+  }
+);
 
 const appSlice = createSlice({
   name: 'app',
   initialState: {
+    userId: null,
     isAdmin: false,
     email: '',
-    favorite: {},
+    favorite: [],
     loading: false,
     allColors: [],
     allCategories: []
@@ -21,18 +41,30 @@ const appSlice = createSlice({
       state.email = '';
       state.isAdmin = false
     },
-    changeLoading(state, {payload}) {
+    changeLoading(state, { payload }) {
       state.loading = payload;
     },
-    changeAllColors(state, {payload}){
+    changeAllColors(state, { payload }) {
       state.allColors = payload;
     },
     changeAllCategories(state, { payload }) {
       state.allCategories = payload;
-    }
+    },
+    changeUserId(state, { payload }) {
+      state.userId = payload;
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(getFavoriten.pending, (state) => {
+      state.loading = true;
+    })
+    builder.addCase(getFavoriten.fulfilled, (state, { payload }) => {
+      state.favorite = payload;
+      state.loading = false;
+    })
   }
 })
 
 export default appSlice.reducer
 
-export const { changeUserRole, changeUserEmail, logout, changeLoading, changeAllCategories, changeAllColors } = appSlice.actions
+export const { changeUserRole, changeUserEmail, logout, changeLoading, changeAllCategories, changeAllColors, changeUserId } = appSlice.actions
